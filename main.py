5f82e6b7b125879ae9b51410044f7e5449a4b612
Anaghjaiswar <jaiswaranagh@gmail.com>
2026-06-30 01:29:08 +0530

    post changed

--- main.py
+++ main.py
@@ -93,4 +93,4 @@ async def query_rag(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8501, reload=True)
